b8542dc358b20a8075ede76e433d5d5ad41bd29f
luis_mndz0@hotmail.com <luis_mndz0@hotmail.com>
2022-05-10 15:44:54 -0500

    first commit

---
+++ lib/model/characters_data.dart
@@ -0,0 +1,18 @@
+import './character.dart';
+
+final chars = [
+  Character(
+      image: 'images/Albert.jpg',
+      name: 'Albert Alemany',
+      cargo: 'Flutter Developer'),
+  Character(
+      image: 'images/Gerard.jpg',
+      name: 'Gerard Guasch',
+      cargo: 'Android Developer'),
+  Character(
+      image: 'images/Ignasi.jpg', name: 'Ignasi Isern', cargo: 'IOS Developer'),
+  Character(
+      image: 'images/Meritxell.jpg',
+      name: 'Meritxell Maimó',
+      cargo: 'React Native Developer'),
+];
